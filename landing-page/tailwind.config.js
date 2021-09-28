e3a4313261118530b4f0a4c61a20d224fdab1c22
Daniel Dietrich <daniel.dietrich@typefox.io>
2021-09-28 15:53:51 +0200

    Added Tailwind CSS boilerplate (dev server, production build, ...)

---
+++ landing-page/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
