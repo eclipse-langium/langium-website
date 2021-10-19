a24301a3f613b728ed5c25ec664721b9a57c5733
Jan Bicker <jan.bicker@typefox.io>
2021-10-19 16:13:52 +0000

    Added background and parallax effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html'],
@@ -6,14 +8,29 @@ module.exports = {
     fontFamily: {
       sans: ['Menlo', 'sans-serif']
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray
+    },
     extend: {
       colors: {
-        emeraldLangium: '#26888C'
-      }
+        emeraldLangium: '#26888C',
+        emeraldLangiumDarker: '#0A4340',
+        emeraldLangiumDarkest: '#042424',
+        accentBlue: '#1FCDEB',
+        accentGreen: '#B6F059',
+        accentViolet: '#D568E7'
+      },
+      backgroundImage: {
+        'office': "url('../assets/office.jpg')"
+       }
     }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [],
 }
