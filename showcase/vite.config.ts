06d379ca6cd0daee0e89b0caed44d4f63cd9fe98
Kai Salmen <kai.salmen@typefox.io>
2022-06-27 18:03:23 +0200

    Use vite for worker and dev

---
+++ showcase/vite.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+const config = defineConfig({
+    build: {
+        target: ['es2020'],
+        rollupOptions: {
+            input: {
+                app: path.resolve(__dirname, 'static/index.html'),
+                statemachine: path.resolve(__dirname, 'static/statemachine/index.html')
+            },
+            output: {
+                esModule: true
+            },
+            external: [
+                //'monaco-editor',
+                //'monaco-editor-comp'
+            ]
+        },
+        commonjsOptions: {
+            include: [/langium/]
+        },
+        outDir: path.resolve(__dirname, 'dist/vite')
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'es2020'
+        }
+    },
+    server: {
+        open: 'static/index.html',
+    }
+});
+
+export default config;
--- showcase/vite.config.worker.ts
+++ showcase/vite.config.worker.ts
@@ -1,16 +1,26 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 const config = defineConfig({
     build: {
+        rollupOptions: {
+            plugins: [
+                // Enable rollup polyfills for production
+                rollupNodePolyFill()
+            ]
+        },
         lib: {
-            entry: path.resolve(__dirname, './src/serverWorker.ts'),
+            entry: path.resolve(__dirname, 'src/serverWorker.ts'),
             name: 'serverWorker',
             fileName: (format) => `serverWorker-${format}.js`,
             formats: ['es']
         },
-        outDir: 'dist/worker',
-        emptyOutDir: false
+        outDir: 'static/libs/worker',
+        emptyOutDir: false,
+        commonjsOptions: {
+            include: [/langium/]
+        }
     }
 });
 
