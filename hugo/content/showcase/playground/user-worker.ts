904e654c24728ce41b4a6fe8aec40a442581bff3
Markus Rudolph <markus.rudolph@typefox.io>
2022-09-20 11:21:42 +0200

    Fix the factory

--- hugo/content/showcase/playground/user-worker.ts
+++ hugo/content/showcase/playground/user-worker.ts
@@ -6,11 +6,11 @@ import { ByPassingMessageWriter, ByPassingMessageReader, PlaygroundWrapper } fro
 const messageWrapper = new PlaygroundWrapper();
 const messageReader = new ByPassingMessageReader(self, messageWrapper);
 const messageWriter = new ByPassingMessageWriter(self, messageWrapper);
+const connection = createConnection(messageReader, messageWriter);
 
 messageReader.listenByPass(message => {
     if(message.type === 'validated') {
-        const connection = createConnection(messageReader, messageWriter);
-        const { shared } = createServicesForGrammar({grammar: message.grammar, sharedModule: {lsp: {Connection: connection}}});
+        const { shared } = createServicesForGrammar({grammar: message.grammar, sharedModule: {lsp: {Connection: () => connection}}});
         startLanguageServer(shared);
     }
 });
