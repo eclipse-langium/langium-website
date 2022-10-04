b3c6d33999c87f1517b79ae004d22419d0fbe6f0
Markus Rudolph <markus.rudolph@typefox.io>
2022-10-04 14:47:30 +0200

    Clear timeout also in error cases

--- hugo/content/playground/langium-worker.ts
+++ hugo/content/playground/langium-worker.ts
@@ -12,29 +12,30 @@ const connection = createConnection(messageReader, messageWriter);
 // Inject the shared services and language-specific services
 const { shared } = createLangiumGrammarServices({ connection, ...EmptyFileSystem });
 
+let timeout: NodeJS.Timeout | undefined;
+
 // by pass other messages that are required to make the playground work
 shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Parsed, () => messageWriter.byPassWrite({type: 'changing'}));
 shared.workspace.DocumentBuilder.onBuildPhase(DocumentState.Validated, ([document]) => {
     const errors = (document.diagnostics ?? []).filter(d => d.severity === DiagnosticSeverity.Error);
     if (errors.length > 0) {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
         return messageWriter.byPassWrite({
             type: 'error',
             errors
         });
     }
-    thottlePassWrite(document.textDocument.getText());
+    throttlePassWrite(document.textDocument.getText());
     return Promise.resolve();
 });
 
-let timeout: NodeJS.Timeout | undefined;
-
-function thottlePassWrite(text: string): void {
+function throttlePassWrite(text: string): void {
     if (timeout) {
-        console.log('Timeout cleared')
         clearTimeout(timeout);
     }
     timeout = setTimeout(() => {
-        console.log('Timeout executed')
         messageWriter.byPassWrite({
             type: 'validated',
             grammar: text
