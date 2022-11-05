56273be2095d5400d584911757c636e8dc235d4d
emilkrebs <onlime@gmx.de>
2022-11-05 02:10:56 +0100

    Started adding a mock preview, so I can work from another device

--- hugo/assets/scripts/statemachine.tsx
+++ hugo/assets/scripts/statemachine.tsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import Editor, { useMonaco } from "@monaco-editor/react";
-import { buildWorkerDefinition } from "monaco-editor-workers";
-import { MonacoEditorLanguageClientWrapper } from 'monaco-editor-wrapper';
+
+interface EventProps {
+  name: string;
+  active: boolean;
+}
+
+class Event extends React.Component<EventProps> {
+  constructor(props: EventProps) {
+    super(props);
+    this.state = {
+      name: props.name,
+      isActive: props.active
+    }
+  }
+
+  setActivate(isItActiveBro: boolean) {
+    this.setState({ isActive: isItActiveBro });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.active ? (
+          <div className="text-emeraldLangium border-2 border-solid border-emeraldLangium rounded-md p-4 text-center text-sm shadow-opacity-50 shadow-[0px_0px_15px_0px] shadow-emeraldLangium">
+            {this.props.name}
+          </div>
+        ) : (
+          <div className="border-2 text-emeraldLangiumDarker border-solid border-emeraldLangiumDarker rounded-md p-4 text-center text-sm shadow-opacity-50 hover:shadow-[0px_0px_10px_0px] hover:shadow-emeraldLangiumDarker">
+            {this.props.name}
+          </div>
+        )}
+
+      </div>
+
+    );
+  }
+}
 
 function Preview() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-     <div className="text-white border-2 border-solid border-emeraldLangium rounded-md p-4 text-center text-sm shadow-opacity-50 hover:shadow-[0px_0px_25px_0px] hover:shadow-emeraldLangiumDarkest">
-      Preview not available
-     </div>
+      <Event name="huen" active={true}></Event>
     </div>
   );
 }
+
+
 
 function App() {
   return (
@@ -20,7 +54,6 @@ function App() {
       <div className="float-left w-1/2 h-full border-r border-white">
         <div className="wrapper relative bg-white dark:bg-gray-900">
           <div className="dark:bg-gray-900" id="monaco-editor-root">
-      
           </div>
         </div>
       </div>
@@ -34,5 +67,5 @@ function App() {
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <App />
+  <App />
 ); 
