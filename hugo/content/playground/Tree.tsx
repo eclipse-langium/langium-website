73fa1384e30365ae96a806d0e96227d8d0161844
Markus Rudolph <markus.rudolph@typefox.io>
2022-12-12 10:38:02 +0100

    Fix layout

--- hugo/content/playground/Tree.tsx
+++ hugo/content/playground/Tree.tsx
@@ -4,96 +4,129 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
- import { AstNode } from "langium";
+import { AstNode } from "langium";
 import React, { FC, useState } from "react";
 import * as ReactDOM from "react-dom/client";
-import { preprocessAstNode, TypeNode } from "./preprocess";
+import { preprocessAstNode, PropertyNode, TypeNode } from "./preprocess";
 import { clsx } from "clsx";
 
 export function render(root: AstNode) {
   const location = document.getElementById("syntax-tree")!;
   const data = preprocessAstNode(root);
-  ReactDOM.createRoot(location).render(<Tree root={data} />);
+  ReactDOM.createRoot(location).render(
+    <ul>
+      <TreeNode root={data} hidden={false} />
+    </ul>
+  );
 }
 
 interface TreeProps {
   root: TypeNode;
-  className?: string;
+  hidden: boolean;
 }
 
-const Tree: FC<TreeProps> = ({ root, className }) => {
-  const [open, setOpen] = useState(true);
+const TreeContent: FC<TreeProps> = ({ root, hidden }) => {
   switch (root.kind) {
     case "boolean":
     case "number":
     case "string":
       return (
-        <span className={className}>
+        <>
           <span className="colon">:&nbsp;</span>
-          <span className="type">{root.kind}</span>
-          <span className="equal">&nbsp;=&nbsp;</span>
           <span className="literal">
-            {root.kind === "string" ? '"' + root.value + '"' : root.value}
+            {hidden
+              ? "..."
+              : root.kind === "string"
+              ? '"' + root.value + '"'
+              : root.value}
           </span>
-        </span>
+        </>
       );
     case "object":
       return (
-        <span className={className}>
-          <span className="value" onClick={() => setOpen((prev) => !prev)}>
-            <span className="type">{root.$type}</span>
+        <>
+          <span className="object">
+            {hidden ? (
+             <span className="opening-brace">&#123;...&#125;</span>
+            ) : (
+              <>
+                <span className="opening-brace">&#123;</span>
+                <ul className="object-body">
+                  {root.properties.map((p, index) => (
+                    <Property
+                      key={index}
+                      p={p}
+                      comma={index !== root.properties.length - 1}
+                    />
+                  ))}
+                </ul>
+                <span className="closing-brace">&#125;</span>
+              </>
+            )}
           </span>
-          <span className={clsx({ hidden: !open })}>
-            <span className="opening-brace">(</span>
-            <ul className="value-body">
-              {root.properties.map((p, index) => (
-                <TreeItem key={index} name={p.name} root={p.type} />
-              ))}
-            </ul>
-            <span className="closing-brace">)</span>
-          </span>
-        </span>
+        </>
       );
     case "array":
       return (
-        <span className={className}>
+        <>
           <span className="colon">:&nbsp;</span>
-          <span className="opening-brace">[</span>
-          <ul className="value-body">
-            {root.children.map((c, index) => (
-              <TreeItem key={index} root={c} />
-            ))}
-          </ul>
-          <div className="closing-brace">]</div>
-        </span>
+          {hidden ? (
+            <span className="opening-brace">{"[...]"}</span>
+          ) : (
+            <>
+              <span className="opening-brace">[</span>
+              <ul className="object-body">
+                {root.children.map((c, index) => (
+                  <li
+                    key={index}
+                    className={clsx("entry toggable inline", {
+                      closed: hidden,
+                    })}
+                  >
+                    <TreeContent root={c} hidden={false} />
+                    {index !== root.children.length - 1 && (
+                      <span className="comma">,&nbsp;</span>
+                    )}
+                  </li>
+                ))}
+              </ul>
+              <span className="closing-brace inline">]</span>
+            </>
+          )}
+        </>
       );
     case "reference":
       return (
-        <span className={className}>
+        <>
           <span className="colon">:&nbsp;</span>
           <span className="link">{root.type.$type}</span>
-        </span>
+        </>
       );
   }
   return <div>???</div>;
 };
 
-const TreeItem: FC<TreeProps & { name?: string }> = ({ root, name }) => {
-  const [open, setOpen] = useState(() => true);
+const TreeNode: FC<TreeProps> = ({ root, hidden }) => {
   return (
-    <li className={"entry toggable"}>
-      <span
-        className={clsx("value", { hidden: !name })}
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        <span className="property">{name}</span>
-      </span>
-      <Tree
-        root={root}
-        className={clsx({
-          hidden: !open,
-        })}
-      />
+    <li className="inline entry">
+      <TreeContent root={root} hidden={hidden} />
     </li>
   );
 };
+
+function Property({ p, comma }: { p: PropertyNode; comma: boolean }) {
+  const [open, setOpen] = useState(true);
+  return (
+    <li className={clsx("entry toggable", { closed: !open })}>
+      <span className={"value"}>
+        <span className="property" onClick={() => setOpen((p) => !p)}>
+          {p.name}
+        </span>
+        <ul className="inline">
+          <TreeNode root={p.type} hidden={!open} />
+        </ul>
+      </span>
+      {comma && <span className="comma">,&nbsp;</span>}
+    </li>
+  );
+}
