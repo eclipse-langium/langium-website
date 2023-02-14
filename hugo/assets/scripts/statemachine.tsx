455b943df77f0687f659eff75955fc65190ce26c
emilkrebs <onlime@gmx.de>
2023-02-14 16:00:44 +0100

    Added nothing, but I improved the code

--- hugo/assets/scripts/statemachine.tsx
+++ hugo/assets/scripts/statemachine.tsx
@@ -1,77 +1,90 @@
-import { MonacoEditorReactComp, monaco, addMonacoStyles } from '@typefox/monaco-editor-react/bundle';
-import { buildWorkerDefinition } from 'monaco-editor-workers';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { deserializeAST, Diagnostic, DocumentChangeResponse } from './ast-tools';
+import {
+  MonacoEditorReactComp,
+  monaco,
+  addMonacoStyles,
+} from "@typefox/monaco-editor-react/bundle";
+import { buildWorkerDefinition } from "monaco-editor-workers";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Diagnostic, DocumentChangeResponse } from "./ast/ast-tools";
+import { StateMachineAST, StateMachineAstNode } from "./ast/statemachine-ast";
 
 buildWorkerDefinition(
   "../../libs/monaco-editor-workers/workers",
   new URL("", window.location.href).href,
   false
 );
-addMonacoStyles('monaco-editor-styles');
+addMonacoStyles("monaco-editor-styles");
 
-
-let dummyData = {
-  events: ["switchCapacity", "next"],
-  states: [
-    {
-      name: "PowerOff",
-      next: "",
-      switchCapacity: "RedLight"
-    },
-    {
-      name: "GreenLight",
-      next: "YellowLight",
-      switchCapacity: "PowerOff"
-    },
-    {
-      name: "YellowLight",
-      next: "RedLight",
-      switchCapacity: "PowerOff"
-    },
-    {
-      name: "RedLight",
-      next: "GreenLight",
-      switchCapacity: "PowerOff"
-    }
-  ],
-  initialState: "PowerOff"
-}
+// let dummyData = {
+//   events: ["switchCapacity", "next"],
+//   states: [
+//     {
+//       name: "PowerOff",
+//       next: "",
+//       switchCapacity: "RedLight"
+//     },
+//     {
+//       name: "GreenLight",
+//       next: "YellowLight",
+//       switchCapacity: "PowerOff"
+//     },
+//     {
+//       name: "YellowLight",
+//       next: "RedLight",
+//       switchCapacity: "PowerOff"
+//     },
+//     {
+//       name: "RedLight",
+//       next: "GreenLight",
+//       switchCapacity: "PowerOff"
+//     }
+//   ],
+//   initialState: "PowerOff"
+// }
 
 const syntaxHighlighting = {
   keywords: [
-    'actions', 'commands', 'end', 'events', 'initialState', 'state', 'statemachine'
+    "actions",
+    "commands",
+    "end",
+    "events",
+    "initialState",
+    "state",
+    "statemachine",
   ],
 
   // The main tokenizer for our languages
   tokenizer: {
     root: [
       // identifiers and keywords
-      [/[a-z_$][\w$]*/, {
-        cases: {
-          '@keywords': 'keyword',
-          '@default': 'identifier'
-        }
-      }],
+      [
+        /[a-z_$][\w$]*/,
+        {
+          cases: {
+            "@keywords": "keyword",
+            "@default": "identifier",
+          },
+        },
+      ],
 
       // whitespace
-      { include: '@whitespace' }
+      { include: "@whitespace" },
     ],
 
     comment: [
-      [/[^\/*]+/, 'comment'],
-      [/\/\*/, 'comment', '@push'],    // nested comment
-      ["\\*/", 'comment', '@pop'],
-      [/[\/*]/, 'comment']
+      [/[^\/*]+/, "comment"],
+      [/\/\*/, "comment", "@push"], // nested comment
+      ["\\*/", "comment", "@pop"],
+      [/[\/*]/, "comment"],
     ],
 
     whitespace: [
-      [/[ \t\r\n]+/, 'white'],
-      [/\/\*/, 'comment', '@comment'],
-      [/\/\/.*$/, 'comment'],
-    ]
-  }
+      [/[ \t\r\n]+/, "white"],
+      [/\/\*/, "comment", "@comment"],
+      [/\/\/.*$/, "comment"],
+    ],
+  },
 } as monaco.languages.IMonarchLanguage;
 
 let currentState;
@@ -90,6 +103,7 @@ interface EventProps {
 
 interface PreviewProps {
   diagnostics?: Diagnostic[];
+  astNode?: StateMachineAstNode;
 }
 
 class State extends React.Component<StateProps, StateProps> {
@@ -98,8 +112,8 @@ class State extends React.Component<StateProps, StateProps> {
     super(props);
     this.state = {
       name: props.name,
-      isActive: props.isActive
-    }
+      isActive: props.isActive,
+    };
     this.stateRef = React.createRef<HTMLInputElement>();
   }
 
@@ -109,7 +123,11 @@ class State extends React.Component<StateProps, StateProps> {
 
   render() {
     return (
-      <div className='cursor-default' onClick={this.props.handleClick} ref={this.stateRef}>
+      <div
+        className="cursor-default"
+        onClick={this.props.handleClick}
+        ref={this.stateRef}
+      >
         {this.state.isActive ? (
           <div className="text-emeraldLangium border-2 border-solid border-emeraldLangium rounded-md p-4 text-center text-sm shadow-opacity-50 shadow-[0px_0px_15px_0px] shadow-emeraldLangium">
             {this.state.name}
@@ -130,7 +148,7 @@ class Event extends React.Component<EventProps, EventProps> {
     this.state = {
       name: props.name,
       isEnabled: props.isEnabled,
-    }
+    };
   }
 
   setEnabled(enabled: boolean) {
@@ -139,74 +157,115 @@ class Event extends React.Component<EventProps, EventProps> {
 
   render() {
     return (
-      <button onClick={this.props.handleClick} disabled={this.state.isEnabled} className="text-white border-2 border-solid bg-emeraldLangiumABitDarker rounded-md p-4 text-center text-sm enabled:hover:shadow-opacity-50 enabled:hover:shadow-[0px_0px_15px_0px] enabled:hover:shadow-emeraldLangium disabled:border-gray-400 disabled:text-gray-400 disabled:bg-emeraldLangiumDarker ">
+      <button
+        onClick={this.props.handleClick}
+        disabled={this.state.isEnabled}
+        className="text-white border-2 border-solid bg-emeraldLangiumABitDarker rounded-md p-4 text-center text-sm enabled:hover:shadow-opacity-50 enabled:hover:shadow-[0px_0px_15px_0px] enabled:hover:shadow-emeraldLangium disabled:border-gray-400 disabled:text-gray-400 disabled:bg-emeraldLangiumDarker "
+      >
         {this.props.name}
       </button>
     );
   }
 }
 
-
 class Preview extends React.Component<PreviewProps, PreviewProps> {
   constructor(props: PreviewProps) {
     super(props);
     this.state = {
       diagnostics: props.diagnostics,
-    }
+      astNode: props.astNode,
+    };
+
+    this.startPreview = this.startPreview.bind(this);
+    this.getNextState = this.getNextState.bind(this);
   }
 
-  startPreview(diagnostics: Diagnostic[]) {
-    this.setState({ diagnostics: diagnostics })
+  startPreview(ast: StateMachineAstNode, diagnostics: Diagnostic[]) {
+    this.setState({ astNode: ast, diagnostics: diagnostics });
+  }
+
+  // find next state by event
+  getNextState(event: string): string {
+    // what the hell was this supposed to do
+    return this.state.astNode.states.find(({ name }) => name === currentState)![event];
   }
 
   render() {
-    // Continue if the code is right
-    console.dir(this.state.diagnostics);
-    if (this.state.diagnostics == null || this.state.diagnostics.length == 0) {
-      let states: State[] = [];
-      let events: Event[] = [];
-      const changeStates = function (state: string) {
-        states.forEach(item => {
-          item.setActive(item.props.name === state);
-        });
-        currentState = state;
-        events.forEach(event => {
-          event.setEnabled(!getNextState(event.props.name));
-        });
-      }
-
-      const getNextState = function (event: string): string {
-        return dummyData.states.find(({ name }) => name === currentState)![event];
-      }
-
+    // check if code has no diagnostics and contains a astNode
+    if (!this.state.astNode) {
+      // Show the exception
       return (
-        <div className="flex flex-col h-full w-full p-4 float-right items-center">
-          <p className='text-white text-lg w-full my-4'>Events</p>
-          <div className='flex flex-wrap w-full gap-2'>
-            {dummyData.events.map((event, index) => {
-              return <Event isEnabled={!getNextState(event)} handleClick={() => changeStates(getNextState(event))} name={event} key={index} ref={event => { events.push(event!) }}></Event>
-            })}
-          </div>
-          <p className='text-white text-lg w-full my-4'>States</p>
-          <div className='flex flex-wrap w-full gap-2 justify-start '>
-            {dummyData.states.map((state, index) => {
-              return <State handleClick={() => changeStates(state.name)} name={state.name} key={index} isActive={currentState == state.name} ref={state => { states.push(state!) }}></State>
-            })}
+        <div className="flex flex-col h-full w-full p-4 justify-start items-center my-10">
+          <div className="text-white border-2 border-solid border-accentRed rounded-md p-4 text-center text-sm cursor-default">
+            No Ast found
           </div>
         </div>
       );
     }
 
-    // Show the exception
+    if (this.state.diagnostics == null || (this.state.diagnostics.length == 0 && this.state.astNode != undefined)) {
+      let states: State[] = [];
+      let events: Event[] = [];
+      const changeStates = function (state: string) {
+        states.forEach((item) => {
+          item.setActive(item.props.name === state);
+        });
+        currentState = state;
+        events.forEach((event) => {
+          event.setEnabled(!this.getNextState(event.props.name));
+        });
+      };
+
     return (
-      <div className="flex flex-col h-full w-full p-4 justify-start items-center my-10">
-        <div className="text-white border-2 border-solid border-accentRed rounded-md p-4 text-center text-sm cursor-default">
-          {this.state.diagnostics.map((diagnostic, i) => {
+      <div className="flex flex-col h-full w-full p-4 float-right items-center">
+        <p className="text-white text-lg w-full my-4">Events</p>
+        <div className="flex flex-wrap w-full gap-2">
+          {this.state.astNode.events.map((event, index) => {
             return (
-              <p>{diagnostic.range}: {diagnostic.message}</p>
-            )
+              <Event
+                isEnabled={!this.getNextState(event)}
+                handleClick={() => changeStates(this.getNextState(event))}
+                name={event}
+                key={index}
+                ref={(event) => {
+                  events.push(event!);
+                }}
+              ></Event>
+            );
           })}
         </div>
+        <p className="text-white text-lg w-full my-4">States</p>
+        <div className="flex flex-wrap w-full gap-2 justify-start ">
+          {this.state.astNode.states.map((state, index) => {
+            return (
+              <State
+                handleClick={() => changeStates(state.name)}
+                name={state.name}
+                key={index}
+                isActive={currentState == state.name}
+                ref={(state) => {
+                  states.push(state!);
+                }}
+              ></State>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  // Show the exception
+  return(
+      <div className = "flex flex-col h-full w-full p-4 justify-start items-center my-10" >
+      <div className="text-white border-2 border-solid border-accentRed rounded-md p-4 text-center text-sm cursor-default">
+        {this.state.diagnostics.map((diagnostic, i) => {
+          return (
+            <p>
+              {diagnostic.range}: {diagnostic.message}
+            </p>
+          );
+        })}
+      </div>
       </div>
     );
   }
@@ -220,6 +279,7 @@ class App extends React.Component<{}> {
 
     // bind 'this' ref for callbacks to maintain parent context
     this.onMonacoLoad = this.onMonacoLoad.bind(this);
+    this.onDocumentChange = this.onDocumentChange.bind(this);
     this.monacoEditor = React.createRef();
     this.preview = React.createRef();
   }
@@ -227,34 +287,36 @@ class App extends React.Component<{}> {
   /**
    * Callback that is invoked when Monaco is finished loading up.
    * Can be used to safely register notification listeners, retrieve data, and the like
-   * 
+   *
    * @throws Error on inability to ref the Monaco component or to get the language client
    */
   onMonacoLoad() {
     // verify we can get a ref to the editor
     if (!this.monacoEditor.current) {
-      throw new Error('Unable to get a reference to the Monaco Editor');
+      throw new Error("Unable to get a reference to the Monaco Editor");
     }
 
     // verify we can get a ref to the language client
-    const lc = this.monacoEditor.current?.getEditorWrapper()?.getLanguageClient();
+    const lc = this.monacoEditor.current
+      ?.getEditorWrapper()
+      ?.getLanguageClient();
     if (!lc) {
-      throw new Error('Could not get handle to Language Client on mount');
+      throw new Error("Could not get handle to Language Client on mount");
     }
 
     // register to receive DocumentChange notifications
-    lc.onNotification('browser/DocumentChange', this.onDocumentChange);
+    lc.onNotification("browser/DocumentChange", this.onDocumentChange);
   }
 
   /**
    * Callback invoked when the document processed by the LS changes
    * Invoked on startup as well
-   * 
+   *
    * @param resp Response data
    */
   onDocumentChange(resp: DocumentChangeResponse) {
     // // decode the received AST
-    // const langiumAst = deserializeAST(resp.content);
+    const statemachineAst = new StateMachineAST().deserializeAST(resp.content);
 
     // const eventNames: string[] = [];
     // for (const event of langiumAst.events) {
@@ -270,22 +332,28 @@ class App extends React.Component<{}> {
     //     console.info(transition.event.ref.name + '\t=>\t' + transition.state.ref.name);
     //   }
     // }
-    this.preview.current?.startPreview(resp.diagnostics);
+    this.preview.current?.startPreview(statemachineAst, resp.diagnostics);
   }
 
   render() {
-    currentState = dummyData.initialState;
     const style = {
-      "paddingTop": "5px",
-      "height": "100%",
-      "width": "100%"
+      paddingTop: "5px",
+      height: "100%",
+      width: "100%",
     };
 
     return (
       <div className="w-full h-full border border-emeraldLangium justify-center self-center flex">
         <div className="float-left w-1/2 h-full border-r border-emeraldLangium">
-          <div className="wrapper relative bg-white dark:bg-gray-900" >
-            <MonacoEditorReactComp ref={this.monacoEditor} onLoad={this.onMonacoLoad} webworkerUri="../showcase/libs/worker/statemachineServerWorker.js" workerName='LS' workerType='classic' languageId="statemachine" text={`// Create your own statemachine here!
+          <div className="wrapper relative bg-white dark:bg-gray-900">
+            <MonacoEditorReactComp
+              ref={this.monacoEditor}
+              onLoad={this.onMonacoLoad}
+              webworkerUri="../showcase/libs/worker/statemachineServerWorker.js"
+              workerName="LS"
+              workerType="classic"
+              languageId="statemachine"
+              text={`// Create your own statemachine here!
 statemachine TrafficLight
 
 events
@@ -311,7 +379,10 @@ end
 state GreenLight
     switchCapacity => PowerOff
     next => YellowLight
-end`} syntax={syntaxHighlighting} style={style} />
+end`}
+              syntax={syntaxHighlighting}
+              style={style}
+            />
           </div>
         </div>
         <div className="float-right w-1/2 h-full" id="preview">
@@ -323,6 +394,4 @@ end`} syntax={syntaxHighlighting} style={style} />
 }
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <App />
-); 
+root.render(<App />);
